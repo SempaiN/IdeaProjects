0f885810120f82a98b8e2b3d3994a46667f64d91
SempaiN <trollnacho.np@gmail.com>
2023-09-20 21:07:53 +0200

    acabao

--- ValidatingNacho/src/main/kotlin/Main.kt
+++ ValidatingNacho/src/main/kotlin/Main.kt
@@ -3,21 +3,45 @@
  * @version 1.3
  */
 
-/*
-   - nombre completo: solo letras, guiones (-) y apóstrofe (') entre 3 y 50 caracteres,
-   - email: formato de dirección de correo electrónico.
-   - dni: 7 u 8 números seguidos de una letra mayúscula.
-   - edad: número de como máximo 3 cifras.
-   - teléfono: número de 9 cifras que debe empezar por 9, 6 o 7.
- */
+
 fun main(args: Array<String>) {
+    //Pido primero todos los datos del usuario
     println("Introduce tu nombre completo")
     val name = readln()
-    val namecheck = Regex(
-        "^[a-z]{3,50}(-|')"
+    val nameCheck = Regex(
+        "^[A-Za-z-']{3,50}"
     )
-    val email = readln();
-    val emailcheck = Regex(
-        "^[a-z]\\W@"
+    println("Introduce tu email")
+    val email = readln()
+    val emailCheck = Regex(
+        "^[a-zA-Z_.]+@[a-z]+.[a-z]{3}$"
     )
+    println(
+        """Introduce tu nif,si tienes NIE la equivalencia es
+        |X = 0
+        |Y = 1
+        |Z = 2
+        """.trimMargin()
+    )
+    val nif = readln()
+    val nifCheck = Regex(
+        "[012]?+[0-9]+[A-Za-z]$"
+    )
+    println("Introduce tu edad")
+    val age = readln()
+    val ageCheck = Regex(
+        "^\\d[0-9]{0,3}$"
+    )
+    println("Introduce tu número de teléfono")
+    val number = readln()
+    val numberCheck = Regex(
+        "[679]+[0-9]{8}$"
+    )
+    //Cuando acabo de pedirlos, los compruebo y digo si el dato es correcto o incorrecto
+    if (name.contains(nameCheck)) println("\nNombre correcto") else println("\nEse nombre es incorrecto")
+    if (email.contains(emailCheck)) println("Email correcto") else println("Formato de email incorrecto")
+    if (nif.contains(nifCheck)) println("NIF correcto") else println("NIF incorrecto")
+    if (age.contains(ageCheck) && age.toInt() < 150) println("Edad Correcta") else println("Edad incorrecta")
+    if (number.contains(numberCheck)) println("Número de teléfono correcto") else println("Número de teléfono incorrecto")
+
 }
