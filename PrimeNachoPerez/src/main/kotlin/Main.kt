33511c3ac745ed133225021a83710c3799d0fe8b
SempaiN <trollnacho.np@gmail.com>
2023-09-25 17:24:23 +0200

    Update Main.kt
    comentarios

--- PrimeNachoPerez/src/main/kotlin/Main.kt
+++ PrimeNachoPerez/src/main/kotlin/Main.kt
@@ -5,13 +5,15 @@
 fun main(args: Array<String>) {
     do {
         print("Introduce un número y te diré si es primo o no\n")
-        var number = readln().toInt()
-        var prime = 0
+        var number = readln().toInt()//Leo el número obligando al usuario a introducir un int
+        var prime = 0 //Uso un contador para saber si es primo
         for (i in 1..number) {
             if (number % i == 0) prime++
         }
+        //Si el resto de la división da 0 se añade un uno al contador
         if (prime == 2) println("Es primo") else println("No es primo")
+        //Si hay más de 2 numeros divisibles no es primo
         println("¿Quieres seguir introduciendo números?")
-        var answer: String = readln().lowercase()
+        var answer: String = readln().lowercase()//Pregunto si quiere seguir comprobando números
     } while (answer == "si")
 }
