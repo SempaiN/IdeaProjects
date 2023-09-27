d4daf71d2a358bef113a9a3ff64dd49b52ad854b
SempaiN <trollnacho.np@gmail.com>
2023-09-28 00:10:16 +0200

    Update Main.kt
    creo que ya está maña se lo enseño a estos y que opinen

--- IgnacioPerezModular/src/main/kotlin/Main.kt
+++ IgnacioPerezModular/src/main/kotlin/Main.kt
@@ -1,6 +1,3 @@
-
-
-
 /**
  * Descripción: Programa que hará de calculadora
  *
@@ -8,25 +5,52 @@
  * @version 1.4
  */
 fun main(args: Array<String>) {
-    println("Dame un nombre")
     var nameCheck = Regex(
         "^[A-Z][a-zA-Z]*\$"
     )
+    var optionCheck = Regex(
+        "^[0-5]$"
+    )
+
+    println("¿Cómo te llamas")
     var name = readln().toString()
-    println(exponent(4,5))
-    greeting(name)
-    do {
-        var option = readln().toDouble()
-        when (option) {
-            //1 ->
-            //2 ->
-            //3 ->
-            else -> break
+
+    while (name.contains(nameCheck)) {
+        greeting(name)
+        println(
+            """¿Qué quieres hacer?
+            |1.- Sumar
+            |2.- Restar
+            |3.- Multiplicar
+            |4.- Dividir
+            |5.- Potencia
+            |0.- Salir
+        """.trimMargin()
+        )
+
+        var option = readln().toString()
+        if (option.toInt() == 0) break
+        if (!option.contains(optionCheck)) {
+            println("Opción no válida")
+            break
         }
-    } while (true)
+        println("Dame el primer número")
+        var number1 = readln().toInt()
+        println("Dame el segundo número")
+        var number2 = readln().toInt()
+
+        when (option.toInt()) {
+            1 -> println("$number1 + $number2 = ${sum(number1, number2)}")
+            2 -> println("$number1 - $number2 = ${subtract(number1, number2)}")
+            3 -> println("$number1 * $number2 = ${multiply(number1, number2)}")
+            4 -> println("$number1 / $number2 = ${divide(number1, number2)}")
+            5 -> println("$number1^$number2 = ${exponent(number1, number2)} ")
+        }
+    }
+    if (!name.contains(nameCheck)) println("Ese nombre no es válido")
 }
 
-fun greeting(name: String) = println("Hola $name")
+fun greeting(name: String) = println("Hola $name\n")
 
 fun sum(number1: Int, number2: Int): Int {
     return number2 + number1
@@ -40,16 +64,17 @@ fun multiply(number1: Int, number2: Int): Int {
     return number1 * number2
 }
 
-fun divide(number1: Int, number2: Int): Any{
-     return number1/number2
+fun divide(number1: Int, number2: Int): Any {
+    if (number2 == 0) return "No se puede divir entre 0"
+    else return number1 / number2
 }
 
-fun exponent(number1:Int ,number2: Int): Int {
+fun exponent(number1: Int, number2: Int): Int {
     var final = number1
-    for (i in 0..<number2){
-        final*=number2
+    for (i in 1..<number2) {
+        final *= number1
     }
-    return  final
+    return final
 }
 
 
